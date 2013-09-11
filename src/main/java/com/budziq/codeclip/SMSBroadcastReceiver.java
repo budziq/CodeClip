52064edde217603dc770b75f6e5c78800b2e53d7
Michał Budzyński <m.budzynski@samsung.com>
2013-09-11 22:03:45 +0200

    added simple notification on sms message receive

--- src/main/java/com/budziq/codeclip/SMSBroadcastReceiver.java
+++ src/main/java/com/budziq/codeclip/SMSBroadcastReceiver.java
@@ -1,9 +1,11 @@
 package com.budziq.codeclip;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import java.lang.String;
 import java.util.Set;
@@ -15,10 +17,27 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "--- sms received! ---");
         Bundle bundle = intent.getExtras();
+        if (bundle != null)
+            handleMessage(context, bundle);
+    }
+
+    private void handleMessage(Context context, Bundle bundle){
         Set<String> set = bundle.keySet();
         for(String s : set){
             Log.i(TAG, s);
         }
-
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+        /*
+        PendingIntent resultPendingIntent;
+        ...
+        mBuilder.setContentIntent(resultPendingIntent);
+        */
+        NotificationManager mNotifyMgr = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(0, mBuilder.build());
     }
 }
